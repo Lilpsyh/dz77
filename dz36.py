1b532e9f2783b57c069f768547ddd4834cc1afa1
aleksander savin <savinsaha6@gmail.com>
2023-09-21 15:01:56 +0500

    vse

--- dz36.py
+++ dz36.py
@@ -1,7 +1,8 @@
 def print_operation_table(num_rows,num_columns):
-    for i in range(num_columns+1):
-        for j in range(num_rows+1):
-            print(f"{i*j} ") 
+    for i in range(1,num_columns+1):
+        for j in range(1,num_rows+1):
+            print(f"{i*j}",end=" ") 
+        print()
 print_operation_table(6,6)
 
 
